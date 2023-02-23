53e727844c59ccd37f6fa80051b178f7c6cf8904
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-24 02:30:49 +0530

    Number Manipulation and F Strings in Python

--- day-002/main.py
+++ day-002/main.py
@@ -1,17 +1,59 @@
-# [Interactive Coding Exercise] BMI Calculator
+# Number Manipulation and F Strings in Python
 
-# Write a program that calculates the Body Mass Index (BMI) from a user's weight and height.
-# The BMI is calculated with the given formula: BMI = weight / (height * height); 
-# where weight is in kg and height is in metre.
+# Rounding off floating point number
+# i.e., if the number is 2.3 we round it off to 2 and if it's 2.7 we round it off to 3.
 
-weight = float(input("Enter your weight in kg: ")) # can be int(), doesn't really matter
-height = float(input("Enter your height in metre: ")) # has to be float() as user will enter the height in decimal
+# In Python, we can round off floating point numbers by using round() function.
 
-BMI = weight / (height ** 2)
+print(8/3) # This will print 2.66666666
 
-bmi_as_whole = int(BMI) # This will convert floating number into whole number
+print(round(8/3)) # This will round off and print 3
 
-print("Your BMI is " + str(bmi_as_whole))
+# To round off decimal places, we do it like this:
+print(round(8/3, 2)) # This will round off to 2 decimal places, so it will print 2.67
+
+print('---------------------')
+
+# Floor division (//) - Converts floating point number into integer without using int() function
+# We can do floor division by using (//)
+
+print(8 / 3) # This will print 2.6666666
+print(8 // 3) # This will print 2
+
+print("---------------------")
+
+
+# Formatting floating point numbers
+print("Format method format()")
+price = 49
+format_price = "{:.2f}".format(price) # "{:.xf}".format(); where x is the number of places to format
+print(format_price) # This will print 49.00
+
+print('-----------------------f-String-----------------------------')
+
+# f-Strings
+# This is used to mix up or concatenate different data types.
+
+# Without f-String
+name = "John"
+age = 14
+
+print("Hello, my name is " + name + " and I am " + str(age) + " years old.")
+
+print('---------------------')
+
+# With f-String
+name = "Peter"
+age = 16
+
+print(f"Hello, my name is {name} and I am {age} years old.")
+
+# To use f-String, use f before the start of the quotation. Now, inside the quotation,
+# we add the curly braces, inside which we will type the variable names. 
+
+# We can also execute Python codes inside the curly braces, with the help of f-String.
+
+print(f"2 + 3 + 4 = {2+4+5}") # This will print: 2 + 4 + 5 = 11
 
 
 
