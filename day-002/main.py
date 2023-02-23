3142611d8b33ad48ad57d1fc59e8649afbbdd145
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-24 02:18:37 +0530

    Python Primitive Data Types

---
+++ day-002/main.py
@@ -0,0 +1,52 @@
+# Python Primitive Data Types
+
+# 1. Strings - String is a piece of characters inside single quotes or double quotes.
+# Eg. "Hello World", "There are 5 apples", "Give me a $5 bill.", "1234".
+
+print("Hello " + "World") # This will print Hello World
+print("123" + "456") # This will print 123456
+
+print('---------------------')
+
+# To find the position of a character of a string from left to right, we do it like this:
+print("Hello"[0])
+# This will print H inside the console, because in programming, we start counting from 0
+print("Hello"[1]) # This will print e inside the console.
+
+print('---------------------')
+
+# 2. Integers - This is a number data type. Integers don't required quotes.
+# Eg.
+print(123) # This will print 123
+print(123 + 456) # This will print 579
+
+print('---------------------')
+
+# We can separate large integers using '_" (underscore) just like we separate large numbers using commas.
+# Eg. 123,456, as 123_456_789
+print(123_456_789)
+
+print('---------------------')
+
+# 3. Float - Numbers with decimal point.
+# Eg. 23.98, 4.567
+
+print(23.09 + 12.89) # This will print 35.980000000000004
+
+print('---------------------')
+
+# 4. Boolean - Logical data types. True or False should begin with capital T and F.
+
+print(2 > 3) # This will print False
+print(5 < 10) # This will print True
+print(True) # This will print True
+print(False) # This will print False
+
+print('---------------------')
+
+
+
+# Connect with me on:
+# Instagram: instagram.com/coder_partha
+# Twitter: twitter.com/hello_partha
+# Github: github.com/parthadeori
