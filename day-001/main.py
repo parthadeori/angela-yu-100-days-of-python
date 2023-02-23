4c54be8bc9fee1d98947b7a58d22586cb090ed45
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-24 02:00:55 +0530

    Printing to the Console in Python

---
+++ day-001/main.py
@@ -0,0 +1,20 @@
+# Printing to the Console in Python
+
+# using print() function
+
+# This will print "Hello World" into the console.
+# print() function always start from the beginning of the line without any space.
+# Click run to execute these lines of code into the console.
+
+print("Hello World") # We can use double quotes as well as single quotes.
+print('Hello World') # Anything inside the quotes are Strings.
+print(23) # Numbers don't require single or double quotes.
+print(2+2) # We can do calculation inside print() function.
+print("3 + 3") # Since the numbers and the operator is inside the quotes, Python will treat it as String.
+
+
+
+# Connect with me on:
+# Instagram: instagram.com/coder_partha
+# Twitter: twitter.com/hello_partha
+# Github: github.com/parthadeori
