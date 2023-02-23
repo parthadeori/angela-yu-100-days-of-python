1e955531d0a39829e15f07d2ae10219b5d4bc54c
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-24 02:23:51 +0530

    [Interactive Coding Exercise] Data Types

--- day-002/main.py
+++ day-002/main.py
@@ -1,65 +1,21 @@
-# Type Error, Type Checking and Type Conversion
+# [Interactive Coding Exercise] Data Types
 
-# len() function doesn't work with integers.
-# print(len(123)) # This will give an error of TypeError
+# Write a program that adds the digits in a 2 digit number. Eg., if the input was 35
+# then the output should be 3 + 5 = 8.
 
-print(len("123")) 
-# This will print 3 because it has 3 characters 
-# but the position of characters will start from 0.
+two_digit_number = input("Type a two digit number: ")
 
-print('--------------------')
+first_digit = two_digit_number[0]
+second_digit = two_digit_number[1]
 
-# Data Type Checking
-# We can check type of data by using the type() function
-
-print(type(123)) # This will print <class 'int'> which means integer
-print(type("Hello123")) # This will print <class 'str'> which means string
-print(type(23.8)) # This will print <class 'float'> which means float
-print(type(True)) # This will print <class 'bool'> which means boolean
+print(first_digit)
+print(second_digit)
 
 print('---------------------')
 
-# Data Types Conversion
-# We cannot concatenate string and integers. To concatenate string with integers,
-# We have to convert either one of them. 
-# To convert integer to string, we can use the str() function
-a = 123
-print(type(a)) # This will print <class 'int'>
-a = str(123)
-print(type(a)) # This will print <class 'str'>
+result = int(first_digit) + int(second_digit)
 
-print('---------------------')
-
-
-# To convert string to integer, we can use the int() function
-b = "456"
-print(type(b)) # This will print <class 'str'>
-b = int("456")
-print(type(b)) # This will print <class 'int'>
-
-print('---------------------')
-
-# Let's use str() and int() function to concatenate string and integers.
-
-num_char = len(input("What is your name? ")) 
-# This will print the number of characters of the user's name
-# This is of class int
-
-num_char_str = str(num_char) 
-# We converted the class int to class str by using str() function
-# Now, num_char_str holds the value as str
-
-print("Your name has " + num_char_str + " characters")
-# We have concatenate strings and integers by converting integers to strings.
-# Enter your name in the console to check the result.
-
-# The below code will give an error.
-# num_char = len(input("What is your name? "))
-# print("Your name has " + num_char + " characters.")
-
-print('---------------------')
-
-# We can convert string like "123" or integers to float by using float() function
+print(result)
 
 
 
