10fc2e9d0b2847695ee57a9cd6d8b890d978834b
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-24 02:32:19 +0530

    [Interactive Coding Exercise] Life in Weeks

--- day-002/main.py
+++ day-002/main.py
@@ -1,59 +1,23 @@
-# Number Manipulation and F Strings in Python
+# [Interactive Coding Exercise] Life in Weeks
 
-# Rounding off floating point number
-# i.e., if the number is 2.3 we round it off to 2 and if it's 2.7 we round it off to 3.
+# Create a program using maths and f-Strings that tells us how many days, weeks and months we have
+# left if we live until 90 years old.
 
-# In Python, we can round off floating point numbers by using round() function.
+# The program will take our current age as the input and output a message with our time left
+# in this format: "You have x days y weeks and z months left",
+# where x, y and z are replaced with the actual calculated numbers.
 
-print(8/3) # This will print 2.66666666
+age = int(input("Enter your age: "))
 
-print(round(8/3)) # This will round off and print 3
+years_remaining = 90 - age
 
-# To round off decimal places, we do it like this:
-print(round(8/3, 2)) # This will round off to 2 decimal places, so it will print 2.67
+days_remaining = years_remaining * 365 # We take 365 days in a year
+weeks_remaining = years_remaining * 52 # We take 52 weeks in a year
+months_remaining = years_remaining * 12 # 12 months in a year
 
-print('---------------------')
+message = f"You have {days_remaining} days {weeks_remaining} weeks and {months_remaining} months left"
 
-# Floor division (//) - Converts floating point number into integer without using int() function
-# We can do floor division by using (//)
-
-print(8 / 3) # This will print 2.6666666
-print(8 // 3) # This will print 2
-
-print("---------------------")
-
-
-# Formatting floating point numbers
-print("Format method format()")
-price = 49
-format_price = "{:.2f}".format(price) # "{:.xf}".format(); where x is the number of places to format
-print(format_price) # This will print 49.00
-
-print('-----------------------f-String-----------------------------')
-
-# f-Strings
-# This is used to mix up or concatenate different data types.
-
-# Without f-String
-name = "John"
-age = 14
-
-print("Hello, my name is " + name + " and I am " + str(age) + " years old.")
-
-print('---------------------')
-
-# With f-String
-name = "Peter"
-age = 16
-
-print(f"Hello, my name is {name} and I am {age} years old.")
-
-# To use f-String, use f before the start of the quotation. Now, inside the quotation,
-# we add the curly braces, inside which we will type the variable names. 
-
-# We can also execute Python codes inside the curly braces, with the help of f-String.
-
-print(f"2 + 3 + 4 = {2+4+5}") # This will print: 2 + 4 + 5 = 11
+print(message)
 
 
 
