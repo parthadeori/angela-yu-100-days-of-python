46edae2e42f45e2197abb7e3195578f1919c04cf
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-24 02:27:48 +0530

    [Interactive Coding Exercise] BMI Calculator

--- day-002/main.py
+++ day-002/main.py
@@ -1,58 +1,17 @@
-# Mathematical Operations in Python
+# [Interactive Coding Exercise] BMI Calculator
 
-# Addition (+)
-print("Addition")
-print(2 + 3)
+# Write a program that calculates the Body Mass Index (BMI) from a user's weight and height.
+# The BMI is calculated with the given formula: BMI = weight / (height * height); 
+# where weight is in kg and height is in metre.
 
-# Subtraction (-)
-print("Subtraction")
-print(34 - 23)
+weight = float(input("Enter your weight in kg: ")) # can be int(), doesn't really matter
+height = float(input("Enter your height in metre: ")) # has to be float() as user will enter the height in decimal
 
-# Multiplication (*)
-print("Multiplication")
-print(2 * 4)
+BMI = weight / (height ** 2)
 
-# Division ( / )
-print("Division")
-print(12 / 4) # Result displayed in float. i.e., 3.0
+bmi_as_whole = int(BMI) # This will convert floating number into whole number
 
-# Exponent or Power ( ** )
-print("Exponent")
-print(2 ** 3) # 2 ** 3 => 2 * 2 * 2 = 8
-
-# Parenthese ()
-print("Parentheses")
-print((3 + 4) * 4) # This will print 28
-
-# Plus Equal (+=)
-print("Plus Equal(+=)")
-x = 3
-x += 3 # This means x + 3
-print(x) # This will print x + 3 = 3 + 3 = 6
-
-# Minus Equal (-=)
-print("Minus Equal (-=)")
-y = 4
-y -= 6
-print(y) # y - 6 = 4 - 6 = -2
-
-# Same goes with (*=) and (/=)
-
-# Remainder (%)
-print("Remainder (%)")
-print(8 % 2) # Remainder will be 0
-print(10 % 3) # Remainder will be 1
-
-# Quotient (//) or Floor Division (see 07-rounding-off.py file)
-print("Quotient (//)")
-print(24 // 12) # The Quotient will be 2 because 12 * 2 = 24, where 2 is the Quotient.
-
-# OPERATORS PRECEDENCE
-
-# The order of precedence in Python is similar to PEMDAS or BIDMAS rule which stands for
-
-# Parentheses > Exponent > Multiplication > Division > Addition > Subtraction
-# Brackets > Indices(Exponent) > Division > Multiplication > Addition > Subtraction 
+print("Your BMI is " + str(bmi_as_whole))
 
 
 
