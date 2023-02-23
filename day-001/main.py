a3c36b2d7983e8d5e43f2ac52c977407f795349c
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-24 02:02:48 +0530

    [Interactive Coding Exercise] Printing

--- day-001/main.py
+++ day-001/main.py
@@ -1,16 +1,8 @@
-# Printing to the Console in Python
+# [Interactive Coding Exercise] Printing
 
-# using print() function
-
-# This will print "Hello World" into the console.
-# print() function always start from the beginning of the line without any space.
-# Click run to execute these lines of code into the console.
-
-print("Hello World") # We can use double quotes as well as single quotes.
-print('Hello World') # Anything inside the quotes are Strings.
-print(23) # Numbers don't require single or double quotes.
-print(2+2) # We can do calculation inside print() function.
-print("3 + 3") # Since the numbers and the operator is inside the quotes, Python will treat it as String.
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
 
 
 
