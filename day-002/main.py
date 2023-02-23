35df42f8af244f01eb7c9c5c016c71b72e6641f4
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-24 02:26:00 +0530

    Mathematical Operations in Python

--- day-002/main.py
+++ day-002/main.py
@@ -1,21 +1,58 @@
-# [Interactive Coding Exercise] Data Types
+# Mathematical Operations in Python
 
-# Write a program that adds the digits in a 2 digit number. Eg., if the input was 35
-# then the output should be 3 + 5 = 8.
+# Addition (+)
+print("Addition")
+print(2 + 3)
 
-two_digit_number = input("Type a two digit number: ")
+# Subtraction (-)
+print("Subtraction")
+print(34 - 23)
 
-first_digit = two_digit_number[0]
-second_digit = two_digit_number[1]
+# Multiplication (*)
+print("Multiplication")
+print(2 * 4)
 
-print(first_digit)
-print(second_digit)
+# Division ( / )
+print("Division")
+print(12 / 4) # Result displayed in float. i.e., 3.0
 
-print('---------------------')
+# Exponent or Power ( ** )
+print("Exponent")
+print(2 ** 3) # 2 ** 3 => 2 * 2 * 2 = 8
 
-result = int(first_digit) + int(second_digit)
+# Parenthese ()
+print("Parentheses")
+print((3 + 4) * 4) # This will print 28
 
-print(result)
+# Plus Equal (+=)
+print("Plus Equal(+=)")
+x = 3
+x += 3 # This means x + 3
+print(x) # This will print x + 3 = 3 + 3 = 6
+
+# Minus Equal (-=)
+print("Minus Equal (-=)")
+y = 4
+y -= 6
+print(y) # y - 6 = 4 - 6 = -2
+
+# Same goes with (*=) and (/=)
+
+# Remainder (%)
+print("Remainder (%)")
+print(8 % 2) # Remainder will be 0
+print(10 % 3) # Remainder will be 1
+
+# Quotient (//) or Floor Division (see 07-rounding-off.py file)
+print("Quotient (//)")
+print(24 // 12) # The Quotient will be 2 because 12 * 2 = 24, where 2 is the Quotient.
+
+# OPERATORS PRECEDENCE
+
+# The order of precedence in Python is similar to PEMDAS or BIDMAS rule which stands for
+
+# Parentheses > Exponent > Multiplication > Division > Addition > Subtraction
+# Brackets > Indices(Exponent) > Division > Multiplication > Addition > Subtraction 
 
 
 
