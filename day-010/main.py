b58d94d20afc53b75914d27de0c39b36b230742a
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-24 20:27:50 +0530

    Print vs. Return

--- day-010/main.py
+++ day-010/main.py
@@ -1,4 +1,4 @@
-# Calculator Part 1: Combining Dictionaries and Functions
+# Print vs. Return
 
 # Add
 def add(n1, n2):
@@ -32,6 +32,11 @@ calculation_function = operations[operation_symbol]
 answer = calculation_function(num1, num2)
 
 print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+operation_symbol = input("Pick another operation: ")
+num3 = int(input("What's the next number?: "))
+calculation_function = operations[operation_symbol]
+answer = calculation_function(answer, num3)
 
 
 
