37f200c4623d058101d50f9328479487ea403c75
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-24 19:58:03 +0530

    Docstrings

--- day-010/main.py
+++ day-010/main.py
@@ -1,38 +1,15 @@
-# [Interactive Coding Exercise] Days in Month
+# Docstrings
 
-# In the starting code, you'll find the solution from the 
-# Leap Year challenge. First, convert this function 
-# is_leap() so that instead of printing "Leap year." or 
-# "Not leap year.", it should return True if it is a leap 
-# year and return False if it is not a leap year. You are 
-# then going to create a function called days_in_month() 
-# which will take a year and a month as inputs.
+def format_name(f_name, l_name):
+    """Take a first and last name and format it to return
+    the title case version of the name."""
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"Result: {formated_f_name} {formated_l_name}"
 
-def is_leap(year):
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                return True
-            else:
-                return False
-        else:
-            return True
-    else:
-        return False
-
-def days_in_month(year, month):
-    if month > 12 or month < 1:
-        return "Invalid month"
-    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    if is_leap(year) and month == 2:
-        return 29
-    return month_days[month - 1]
-
-year = int(input("Enter a year: "))
-month = int(input("Enter a month: "))
-days = days_in_month(year, month)
-print(days)
-
+print(format_name("coder", "PARTHA"))
 
 
 
