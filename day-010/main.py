32a1a4145ef1aeddae38a4fa3e27d21d73268d54
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-24 20:18:42 +0530

    Calculator Part 1: Combining Dictionaries and Functions

--- day-010/main.py
+++ day-010/main.py
@@ -1,15 +1,37 @@
-# Docstrings
+# Calculator Part 1: Combining Dictionaries and Functions
 
-def format_name(f_name, l_name):
-    """Take a first and last name and format it to return
-    the title case version of the name."""
-    if f_name == "" or l_name == "":
-        return "You didn't provide valid inputs."
-    formated_f_name = f_name.title()
-    formated_l_name = l_name.title()
-    return f"Result: {formated_f_name} {formated_l_name}"
+# Add
+def add(n1, n2):
+    return n1 + n2
 
-print(format_name("coder", "PARTHA"))
+# Subtract
+def subtract(n1, n2):
+    return n1 - n2
+
+# Multiply
+def multiply(n1, n2):
+    return n1 * n2
+
+# Divide
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+num1 = int(input("What's the first number?: "))
+for symbol in operations:
+    print(symbol)
+operation_symbol = input("Pick an operation from the line above: ")
+num2 = int(input("What's the second number?: "))
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
 
 
 
