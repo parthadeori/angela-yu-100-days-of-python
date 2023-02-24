9841ce82e73c3236c708328b69981c57358e05d7
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-24 19:36:21 +0530

    Functions with Outputs

---
+++ day-010/main.py
@@ -0,0 +1,16 @@
+# Functions with Outputs
+
+def format_name(f_name, l_name):
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+
+    return f"{formated_f_name} {formated_l_name}"
+
+print(format_name("coder", "PARTHA"))
+
+
+
+# Connect with me on:
+# Instagram: instagram.com/coder_partha
+# Twitter: twitter.com/hello_partha
+# Github: github.com/parthadeori
