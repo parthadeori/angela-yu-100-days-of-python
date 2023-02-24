18f59c3051ae5ff778710e1756676b8f900fb33b
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-24 19:37:38 +0530

    Multiple return values

--- day-010/main.py
+++ day-010/main.py
@@ -1,12 +1,13 @@
-# Functions with Outputs
+# Multiple return values
 
 def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
+    return f"Result: {formated_f_name} {formated_l_name}"
 
-    return f"{formated_f_name} {formated_l_name}"
-
-print(format_name("coder", "PARTHA"))
+print(format_name(input("What is your first name? "), input("What is your last name? ")))
 
 
 
