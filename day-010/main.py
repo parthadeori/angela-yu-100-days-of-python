b0aca2944cdf15f52f8c6e97dd36a668224a92e7
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-24 20:51:31 +0530

    While Loops, Flags and Recursion

--- day-010/main.py
+++ day-010/main.py
@@ -1,4 +1,4 @@
-# Print vs. Return
+# While Loops, Flags and Recursion
 
 # Add
 def add(n1, n2):
@@ -23,20 +23,28 @@ operations = {
     "/": divide
 }
 
-num1 = int(input("What's the first number?: "))
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation from the line above: ")
-num2 = int(input("What's the second number?: "))
-calculation_function = operations[operation_symbol]
-answer = calculation_function(num1, num2)
+def calculator():
+    num1 = int(input("What's the first number?: "))
+    for symbol in operations:
+        print(symbol)
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+    should_continue = True
 
-operation_symbol = input("Pick another operation: ")
-num3 = int(input("What's the next number?: "))
-calculation_function = operations[operation_symbol]
-answer = calculation_function(answer, num3)
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = int(input("What's the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+calculator()
 
 
 
