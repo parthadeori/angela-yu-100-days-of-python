0b2358982210e807573ca836ee992449ddee1924
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-25 21:17:50 +0530

    Nested if statements and elif statements

--- day-003/main.py
+++ day-003/main.py
@@ -15,6 +15,27 @@ if(height >= 120):
 else:
     print("Sorry you have to grow taller before you can ride.")
 
+# elif Statement - If we have multiple 'if' statements, then we use the 'elif' statements.
+
+#'elif' statements required condition to be passed on.
+# We can add many 'elif' statements we want.
+# In this case, we are using 'elif' statement nested inside the 'if' statement.
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if(height >= 120): # If this condition is True, this 'if' block code will execute.
+    print("You can ride the rollercoaster!")
+    age = int(input("What is your age? "))
+    if(age < 12): # If 'age' is less than 12, this code block will execute.
+        print("People under 12 years should pay $5")
+    elif(age <= 18): # If 'age' is lesser or equal to 18, then this code will execute. 
+        print("People under 12-18 years should pay $7.")
+    else: # If 'age' is 19 or more, then this code will execute
+        print("People over 18 years should pay $12.")
+else:
+    print("Sorry you have to grow taller before you can ride.")
+
 
 
 # Connect with me on:
