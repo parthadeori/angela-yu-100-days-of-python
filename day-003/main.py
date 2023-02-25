e09c3dae7dfbf9a3eb13e41fd300c58ece5a81b9
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-25 21:15:53 +0530

    Nested if statements and elif statements

--- day-003/main.py
+++ day-003/main.py
@@ -1,12 +1,19 @@
-# [Interactive Coding Exercise] Odd or Even? Introducing the Modulo
+# Nested if statements and elif statements
 
-# Write a program that works out whether if a given number is an odd or even number.
+# This if/else statement is inside the 'if' statement block.
+# If a condition is True, Python will check for the if/else statement inside the 'if' block of code.
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
 
-number = int(input("Enter a number: "))
-if(number % 2 == 0):
-    print(f"{number} is an even number.")
+if(height >= 120):
+    print("You can ride the rollercoaster!")
+    age = int(input("What is your age? "))
+    if(age <= 18): # Nested if/else statement start from here in this case.
+        print("Please pay $7.")
+    else:
+        print("Please pay $12.")
 else:
-    print(f"{number} is an odd number.")
+    print("Sorry you have to grow taller before you can ride.")
 
 
 
