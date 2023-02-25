f36e76a80be32953bfec181c15377f0deb8d7067
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-25 21:32:30 +0530

    [Interactive Coding Exercise] Love Calculator

--- day-003/main.py
+++ day-003/main.py
@@ -1,39 +1,53 @@
-# Logical Operators
+# [Interactive Coding Exercise] Love Calculator
 
-# Allow us to check multiple conditions on the same line of code.
-# There are three Logical Operators: and, or, not
+# You are going to write a program that tests the compatibility between two people.
+# To work out the love score between two people:
+# Take both people's names and check for the number of times the letters in the word 'TRUE' occurs. 
+# Then check for the number of times the letters in the word 'LOVE' occurs. 
+# Then combine these numbers to make a 2 digit number. 
 
-# 'and' - Returns True if both statements are true
-a = 12
-print(a > 15 and a > 10) # This will print False
-print(a > 10 and a < 15) # This will print True
+# For Love Scores less than 10 or greater than 90, the message should be:
+# "Your score is 'x', you go together like coke and mentos."
 
-# 'or' - Returns True if one of the statements is true
-print(a > 15 or a > 10) # This will print True
-print(a > 15 or a > 17) # This will print False
+# For Love Scores between 40 and 50, the message should be:
+# "Your score is 'y', you are alright together."
 
-# 'not' - Reverse the result, returns False if the result is true
-print(not(a > 16)) # This will print True
-print(not(a > 15 or a < 20)) # This will print False
+# Otherwise, the message will just be their score. e.g.:
+# "Your score is 'z'."
 
-##################################################################
+# Write your code below this line:
 
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
+name1 = input("Enter your name: ") # Peter
+name2 = input("Enter your lover's name: ") # Rose
 
-if(height >= 120):
-    print("You can ride the rollercoaster!")
-    age = int(input("What is your age? "))
-    if(age < 12):
-        print("People under 12 years should pay $5")
-    elif(age <= 18):
-        print("People under 12-18 years should pay $7.")
-    elif(age >= 45 and age <= 55): # Logical Operator Condition added
-        print("You get a free ride.")
-    else:
-        print("People over 18 years should pay $12.")
+combined_names = name1 + name2 # PeterRose
+lower_case_str = combined_names.lower() # peterrose
+
+t = lower_case_str.count("t") # 1
+r = lower_case_str.count("r") # 2
+u = lower_case_str.count("u") # 0
+e = lower_case_str.count("e") # 3
+
+true = t + r + u + e # 6
+
+l = lower_case_str.count("l") # 0
+o = lower_case_str.count("o") # 1
+v = lower_case_str.count("v") # 0
+e = lower_case_str.count("e") # 3
+
+love = l + o + v + e # 4
+
+love_score = str(true) + str(love) # "6" + "4" = "64"
+love_score_int = int(love_score) # 64
+
+# For Love Scores less than 10 or greater than 90
+if(love_score_int < 10 or love_score_int > 90):
+    print(f"Your love score is {love_score}, you go together like coke and mentos.")
+elif(love_score_int >= 40 and love_score_int <= 50): # For Love Scores between 40 and 50
+    print(f"Your love score is {love_score}, you are alright together.")
 else:
-    print("Sorry you have to grow taller before you can ride.")
+    print(f"Your love score is {love_score}")
+
 
 
 
