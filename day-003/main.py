ff0f32783206a29d934241018585b36af5110ee9
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-25 21:30:09 +0530

    Logical Operators

--- day-003/main.py
+++ day-003/main.py
@@ -16,6 +16,25 @@ print(a > 15 or a > 17) # This will print False
 print(not(a > 16)) # This will print True
 print(not(a > 15 or a < 20)) # This will print False
 
+##################################################################
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if(height >= 120):
+    print("You can ride the rollercoaster!")
+    age = int(input("What is your age? "))
+    if(age < 12):
+        print("People under 12 years should pay $5")
+    elif(age <= 18):
+        print("People under 12-18 years should pay $7.")
+    elif(age >= 45 and age <= 55): # Logical Operator Condition added
+        print("You get a free ride.")
+    else:
+        print("People over 18 years should pay $12.")
+else:
+    print("Sorry you have to grow taller before you can ride.")
+
 
 
 # Connect with me on:
