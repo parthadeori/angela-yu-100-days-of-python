24b2bebbe355694c0d02d7cc510ca0d684b2d7b0
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-25 21:25:38 +0530

    Multiple If Statements in Succession

--- day-003/main.py
+++ day-003/main.py
@@ -1,40 +1,33 @@
-# [Interactive Coding Exercise] Leap Year
+# Multiple If Statements in Succession
 
-# Write a program that works out whether if a given year is a leap year. 
+# Now we are going to add another if statement for which we
+# ask the user, if they want to take photos while in the rollercoaster.
 
-year = int(input("Enter a year: "))
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0 # This 'bill' variable is created so that it calculates the final amount at the end
 
-# EXPLANATORY VERSION - Comment the simple version to try this.
-
-if(year % 4 == 0):
-    print(f"{year} is clearly divisible by 4. (Leap year). Checking second condition...")
-    if(year % 100 == 0):
-        print(f"{year} is not divisible by 100 clearly. (Not a leap year). Checking final condition...")
-        if(year % 400 == 0):
-            print(f"{year} is finally divided by 400 clearly. (Leap year).")
-            print(f"So, {year} is a Leap year.")
-        else:
-            print(f"{year} is not divisible by 400. (Not a leap year).")
-            print(f"Hence, {year} is not a leap year")
+if(height >= 120):
+    print("You can ride the rollercoaster!")
+    age = int(input("What is your age? "))
+    if(age < 12):
+        bill = 5
+        print("Child tickets are $5")
+    elif(age <= 18):
+        bill = 7
+        print("Youth tickets are $7.")
     else:
-        print(f"The second condition is omitted since {year} is not divisible by 100 clearly.")
-        print(f"Hence, {year} is a Leap year.")
+        bill = 12
+        print("Adult tickets are $12.")
+
+    wants_photo = input("Do you wanna photo taken? Y or N: ") # here we have created another variable
+    if(wants_photo == "Y"): # This is another 'if' statement for another condition
+        bill += 3 # bill = bill + 3
+    
+    print(f"Your final bill is ${bill}")
+
 else:
-    print(f"{year} is not divisible by 4. So, {year} is Not a leap year.")
-
-
-## SIMPLE VERSION WITHOUT EXPLANATION - Comment the explanatory version to try this.
-
-# if(year % 4 == 0):
-#     if(year % 100 == 0):
-#         if(year % 400 == 0):
-#             print("Leap year")
-#         else:
-#             print("Not a leap year")
-#     else:
-#         print("Leap year")
-# else:
-#     print("Not a leap year.")
+    print("Sorry you have to grow taller before you can ride.")
 
 
 
