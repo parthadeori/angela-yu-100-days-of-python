21614e4c80acd34249e295eeb8ca5fc41f861dc2
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-25 21:33:57 +0530

    Day 3 Project: Treasure Island

--- day-003/main.py
+++ day-003/main.py
@@ -1,52 +1,48 @@
-# [Interactive Coding Exercise] Love Calculator
+# Day 3 Project: Treasure Island
 
-# You are going to write a program that tests the compatibility between two people.
-# To work out the love score between two people:
-# Take both people's names and check for the number of times the letters in the word 'TRUE' occurs. 
-# Then check for the number of times the letters in the word 'LOVE' occurs. 
-# Then combine these numbers to make a 2 digit number. 
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
 
-# For Love Scores less than 10 or greater than 90, the message should be:
-# "Your score is 'x', you go together like coke and mentos."
+choice1 = input("You're at the crossroad, where do you want to go? Type 'left' or 'right': ").lower()
 
-# For Love Scores between 40 and 50, the message should be:
-# "Your score is 'y', you are alright together."
-
-# Otherwise, the message will just be their score. e.g.:
-# "Your score is 'z'."
-
-# Write your code below this line:
-
-name1 = input("Enter your name: ") # Peter
-name2 = input("Enter your lover's name: ") # Rose
-
-combined_names = name1 + name2 # PeterRose
-lower_case_str = combined_names.lower() # peterrose
-
-t = lower_case_str.count("t") # 1
-r = lower_case_str.count("r") # 2
-u = lower_case_str.count("u") # 0
-e = lower_case_str.count("e") # 3
-
-true = t + r + u + e # 6
-
-l = lower_case_str.count("l") # 0
-o = lower_case_str.count("o") # 1
-v = lower_case_str.count("v") # 0
-e = lower_case_str.count("e") # 3
-
-love = l + o + v + e # 4
-
-love_score = str(true) + str(love) # "6" + "4" = "64"
-love_score_int = int(love_score) # 64
-
-# For Love Scores less than 10 or greater than 90
-if(love_score_int < 10 or love_score_int > 90):
-    print(f"Your love score is {love_score}, you go together like coke and mentos.")
-elif(love_score_int >= 40 and love_score_int <= 50): # For Love Scores between 40 and 50
-    print(f"Your love score is {love_score}, you are alright together.")
+if(choice1 == 'left'):
+    choice2 = input("You've come to a lake. There is an island in the middle of the lake. \nType 'wait' or 'swim': ").lower()
+    if(choice2 == 'wait'):
+        choice3 = input("You've arrived at the island unharmed. There is a house with 3 doors. \nOne red, one blue and one yellow. Which color do you choose? ").lower()
+        if(choice3 == 'red'):
+            print("It's a room full of fire. Game Over.")
+        elif(choice3 == 'blue'):
+            print("The monster attacked you. Game Over.")
+        elif(choice3 == 'yellow'):
+            print("You've found the treasure. You Win.")
+        else:
+            print("You chose a door that doesn't exist. Game Over.")
+    else:
+        print("You got attacked by the crocodiles. Game Over.")
 else:
-    print(f"Your love score is {love_score}")
+    print("You fell into a hole. Game Over.")
 
 
 
