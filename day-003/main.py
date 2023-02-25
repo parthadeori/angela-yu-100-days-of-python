c62ed398e2e77bbc9ddbc0c89ed5d4b46ddf0afb
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-25 21:12:01 +0530

    Control Flow with if / else and Conditional Operators

---
+++ day-003/main.py
@@ -0,0 +1,29 @@
+# Control Flow with if / else and Conditional Operators
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+# Now we are going to check whether if the height of the user is greater than 120 cm or not
+
+if(height >= 120):
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry you have to grow taller before you can ride.")
+
+# How this if/else statements work?
+# To use the if/else statement: Firstly we have to the 'if' keyword
+# Then we put the condition inside parentheses.
+# The condition here is the 'height', which the user will input.
+# Then we use the colon (:)
+# When we hit enter, Python will automatically indent the next line, which is the code block for the 'if' statement.
+# If the condition is true, Python will execute the 'if' block
+# And if the condition is false, Python will execute the 'else' block.
+# 'else' statement do not require any condition to be passed on.
+# The 'else' statement is written on the next line and the indentation of 'if' and 'else' statement should be same.
+
+
+
+# Connect with me on:
+# Instagram: instagram.com/coder_partha
+# Twitter: twitter.com/hello_partha
+# Github: github.com/parthadeori
