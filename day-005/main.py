ac6b326be18117d5e37da9eb0030a951137f983b
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-26 23:49:20 +0530

    Day 5 Project: Create a Password Generator

--- day-005/main.py
+++ day-005/main.py
@@ -1,21 +1,39 @@
-# [Interactive Coding Exercise] The FizzBuzz Job Interview Question
+# Day 5 Project: Create a Password Generator
+# Easy Level
 
-# Write a program that automatically prints the solution to the FizzBuzz game.
-# The program should print each number from 1 to 100 in turn.
-# When the number is divisible by 3 then instead of printing the number it should print "Fizz".
-# `When the number is divisible by 5, then instead of printing the number it should print "Buzz".` 
-# `And if the number is divisible by both 3 and 5 e.g. 15 then instead of 
-# the number it should print "FizzBuzz"`
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
+'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
+'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 
+'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-for number in range(1, 101):
-    if number % 3 == 0 and number % 5 == 0:
-        print("FizzBuzz")
-    elif number % 3 == 0:
-        print("Fizz")
-    elif number % 5 == 0:
-        print("Buzz")
-    else:
-        print(number)
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+# Easy Level - Order not randomised:
+# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+password = ""
+
+for char in range(1, nr_letters + 1):
+    random_char = random.choice(letters)
+    password = password + random_char
+
+for char in range(1, nr_symbols + 1):
+    random_char = random.choice(symbols)
+    password = password + random_char
+
+for char in range(1, nr_numbers + 1):
+    random_char = random.choice(numbers)
+    password += random_char
+
+print(password)
 
 
 
