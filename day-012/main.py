f47e4e1be4fa87620e8ceae367c27165f6f7164b
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-26 21:04:29 +0530

    Does Python Have Block Scope?

--- day-012/main.py
+++ day-012/main.py
@@ -1,44 +1,16 @@
-# Namespaces: Local vs. Global Scope
+# Does Python Have Block Scope?
+
+# There is no Block Scope in Python
+
+game_level = 3
+enemies = ["Skeleton", "Zombie", "Alien"]
+if game_level < 5:
+    new_enemy = enemies[0]
+
+print(new_enemy)
 
 ################### Scope ###################
 
-enemies = 1
-
-def increase_enemies():
-    enemies = 2
-    print(f"enemies inside function: {enemies}")
-
-increase_enemies()
-print(f"enemies outside function: {enemies}")
-
-################### Local Scope ###################
-
-def drink_potion():
-    potion_strength = 2
-    print(potion_strength)
-
-drink_potion()
-
-################### Global Scope ###################
-
-player_health = 10
-
-def drink_potion():
-    potion_strength = 2
-    print(player_health)
-
-drink_potion()
-
-player_health = 10
-
-def game():
-    def drink_potion():
-        potion_strength = 2
-        print(player_health)
-
-    drink_potion()
-
-print(player_health)
 
 
 
