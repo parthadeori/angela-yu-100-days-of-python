01d0f128d52a4e09a28eb3d3010331bab752530c
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-26 23:12:27 +0530

    Random Module

---
+++ day-004/main.py
@@ -0,0 +1,26 @@
+# Random Module
+
+# To use the Random module, first we have to import the Random module.
+
+import random
+
+# randint(a, b) - Returns a random integer between a and b (both inclusive)
+
+random_integer = random.randint(1, 10) # This will generate a random integer from 1 and 10
+print(random_integer)
+
+# random.random() - Return a random floating number between [0.0 - 1.0) not including 1.0
+
+random_float = random.random()
+print(random_float)
+
+# Create a random floating number between 1 and 5
+randomFloat = random.random() * 5
+print(randomFloat) # This will generate a random float number between 0.000000 to 4.999999
+
+
+
+# Connect with me on:
+# Instagram: instagram.com/coder_partha
+# Twitter: twitter.com/hello_partha
+# Github: github.com/parthadeori
