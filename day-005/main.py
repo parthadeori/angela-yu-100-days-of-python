1b1f7c6d127c695cadfbe805d81c40462125b2bc
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-26 23:44:55 +0530

    for loops and the range() function

--- day-005/main.py
+++ day-005/main.py
@@ -1,18 +1,12 @@
-# [Interactive Coding Exercise] High Score
+# for loops and the range() function
 
-# You are going to write a program that calculates the highest score from a List of scores. 
+for number in range(1, 10, 3):
+    print(number)
 
-student_scores = input("Input a list of student scores ").split()
-for n in range(0, len(student_scores)):
-  student_scores[n] = int(student_scores[n])
-print(student_scores)
-
-highest_score = 0
-for score in student_scores:
-    if score > highest_score:
-        highest_score = score
-
-print(f"The highest score in the class is: {highest_score}")
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
 
 
 
