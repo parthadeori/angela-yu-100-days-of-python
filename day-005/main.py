f42745c958a0e484ff55929396cadd5819dd68d7
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-26 23:40:02 +0530

    Using the for loop with Python Lists

---
+++ day-005/main.py
@@ -0,0 +1,14 @@
+# Using the for loop with Python Lists
+
+fruits = ["Apple",  "Peach", "Pear"]
+
+for item in fruits:
+    print(item)
+    print(item + " Pie")
+
+
+
+# Connect with me on:
+# Instagram: instagram.com/coder_partha
+# Twitter: twitter.com/CoderPartha
+# Github: github.com/parthadeori
