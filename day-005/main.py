57fe51ebc168873876f2683077297729ab0349f8
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-26 23:43:32 +0530

    [Interactive Coding Exercise] High Score

--- day-005/main.py
+++ day-005/main.py
@@ -1,25 +1,18 @@
-# [Interactive Coding Exercise] Average Height
+# [Interactive Coding Exercise] High Score
 
-# You are going to write a program that calculates the 
-# average student height from a List of heights. 
+# You are going to write a program that calculates the highest score from a List of scores. 
 
-student_heights = input("Input a list of student heights ").split()
-for n in range(0, len(student_heights)):
-  student_heights[n] = int(student_heights[n])
-print(student_heights)
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
 
-total_height = 0
-for height in student_heights:
-    total_height = total_height + height
-print(total_height) 
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
 
-number_of_students = 0
-for student in student_heights:
-    number_of_students += 1    # number_of_students = number_of_students + 1
-print(number_of_students)
-
-average_height = round(total_height/number_of_students)
-print(average_height)
+print(f"The highest score in the class is: {highest_score}")
 
 
 
