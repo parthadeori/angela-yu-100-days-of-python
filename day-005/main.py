d3b6a5adb979482ac80f35ba6b7a63231f082f41
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-26 23:47:46 +0530

    [Interactive Coding Exercise] The FizzBuzz Job Interview Question

--- day-005/main.py
+++ day-005/main.py
@@ -1,11 +1,21 @@
-# [Interactive Coding Exercise] Adding Even Numbers
+# [Interactive Coding Exercise] The FizzBuzz Job Interview Question
 
-# Write a program that calculates the sum of all the even numbers from 1 to 100.
+# Write a program that automatically prints the solution to the FizzBuzz game.
+# The program should print each number from 1 to 100 in turn.
+# When the number is divisible by 3 then instead of printing the number it should print "Fizz".
+# `When the number is divisible by 5, then instead of printing the number it should print "Buzz".` 
+# `And if the number is divisible by both 3 and 5 e.g. 15 then instead of 
+# the number it should print "FizzBuzz"`
 
-total = 0
-for number in range(2, 101, 2):
-    total = total + number
-print(total)
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
 
 
 
