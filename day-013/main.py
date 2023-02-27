a73fdcf1bcf15408e77489649d5635ec173736c9
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-27 20:35:48 +0530

    Describe the Problem

---
+++ day-013/main.py
@@ -0,0 +1,19 @@
+# Describe the Problem
+
+# The problem in the following code is that, when calling my_function(), "You got it"
+# should print in the console. But nothing is printed on the console while executing 
+# the code. Try to debug the code and see if "You got it" gets printed in the console,
+# by just changing a value.
+
+def my_function():
+  for i in range(1, 20):
+    if i == 20:
+      print("You got it")
+my_function()
+
+
+
+# Connect with me on:
+# Instagram: instagram.com/coder_partha
+# Twitter: twitter.com/CoderPartha
+# Github: github.com/parthadeori
