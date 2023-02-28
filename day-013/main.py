63cc404c49ee01560e756fc41f65596b4294b31b
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-28 21:39:04 +0530

    [Interactive Coding Exercise] Debugging Odd or Even - Solution

--- day-013/main.py
+++ day-013/main.py
@@ -1,10 +1,10 @@
-# [Interactive Coding Exercise] Debugging Odd or Even
+# [Interactive Coding Exercise] Debugging Odd or Even - Solution
 
 # Modify the code to fix the problem.
 
 number = int(input("Which number do you want to check? "))
 
-if number % 2 = 0:
+if number % 2 == 0:
   print("This is an even number.")
 else:
   print("This is an odd number.")
