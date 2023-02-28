23b32f78a75258053f96e2e07adc20bf918da0fd
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-28 22:03:38 +0530

    [Interactive Coding Exercise] Debugging FizzBuzz - Solution

--- day-013/main.py
+++ day-013/main.py
@@ -1,16 +1,16 @@
-# [Interactive Coding Exercise] Debugging FizzBuzz
+# [Interactive Coding Exercise] Debugging FizzBuzz - Solution
 
 # Modify the code to fix the program
 
 for number in range(1, 101):
-  if number % 3 == 0 or number % 5 == 0:
+  if number % 3 == 0 and number % 5 == 0:
     print("FizzBuzz")
-  if number % 3 == 0:
+  elif number % 3 == 0:
     print("Fizz")
-  if number % 5 == 0:
+  elif number % 5 == 0:
     print("Buzz")
   else:
-    print([number])
+    print(number)
 
 
 
