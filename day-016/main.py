0f5b862d4999547bcd5188dc29f5164fa92ea452
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-03-07 22:53:22 +0530

    Practice Modifying Object Attributes and Calling Methods

--- day-016/main.py
+++ day-016/main.py
@@ -1,11 +1,9 @@
-from turtle import Turtle, Screen
+from prettytable import PrettyTable
 
-timmy = Turtle()
-timmy.shape("turtle")
-timmy.color("coral")
-timmy.forward(100)
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
 
+print(table.align)
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+print(table)
