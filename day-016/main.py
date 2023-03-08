d97c9b36685514760faf95e18cf8e0478ad6607d
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-03-08 23:41:01 +0530

    initial commit

--- day-016/main.py
+++ day-016/main.py
@@ -1,9 +1,3 @@
-from prettytable import PrettyTable
-
-table = PrettyTable()
-table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
-table.add_column("Type", ["Electric", "Water", "Fire"])
-
-print(table.align)
-
-print(table)
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
