a59f0a32aa9566d2501270e9c22329faec933bfe
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-03-09 15:23:39 +0530

    Working with Attributes, Class Constructors and the __init__() Function

--- day-017/main.py
+++ day-017/main.py
@@ -1,5 +1,10 @@
 class User:
-    pass
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
 
 
-user_1 = User()
+user_1 = User("001", "partha")
+user_2 = User("002", "john")
+
+print(user_1.username)
