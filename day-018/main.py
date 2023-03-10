34c19fcc09004fbb7386078b961fe1d39fabfad1
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-03-10 23:38:40 +0530

    Turtle Challenge 3 - Drawing Different Shapes

--- day-018/main.py
+++ day-018/main.py
@@ -1,12 +1,19 @@
 from turtle import Turtle, Screen
+import random
 
 tim = Turtle()
 
-for _ in range(15):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+colors = ["cornflower blue", "turquoise", "medium sea green", "green yellow", "yellow", "dark orange", "red", "deep pink", "orchid", "medium violet red"]
+
+def draw_shape(num_of_sides):
+    angle = 360/num_of_sides
+    for _ in range(num_of_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for shape_side_n in range(3, 11):
+    tim.color(random.choice(colors))
+    draw_shape(shape_side_n)
 
 
 
