09c0117a86c4bd6419826c38e90a7b981ee42a9b
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-03-10 23:19:32 +0530

    Turtle Challenge 1 - Draw a Square

--- day-018/main.py
+++ day-018/main.py
@@ -1,10 +1,10 @@
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("red")
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
+tim = Turtle()
+
+for _ in range(4):
+    tim.forward(100)
+    tim.left(90)
 
 
 
