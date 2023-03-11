1c90c57ab9afdc75cac2da40ca030bd80624c8b2
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-03-11 23:33:56 +0530

    Turtle Challenge 4 - Generate a Random Walk

--- day-018/main.py
+++ day-018/main.py
@@ -4,17 +4,14 @@ import random
 tim = Turtle()
 
 colors = ["cornflower blue", "turquoise", "medium sea green", "green yellow", "yellow", "dark orange", "red", "deep pink", "orchid", "medium violet red"]
+directions = [0, 90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
 
-def draw_shape(num_of_sides):
-    angle = 360/num_of_sides
-    for _ in range(num_of_sides):
-        tim.forward(100)
-        tim.right(angle)
-
-for shape_side_n in range(3, 11):
+for _ in range(200):
     tim.color(random.choice(colors))
-    draw_shape(shape_side_n)
-
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
 
 
 screen = Screen()
